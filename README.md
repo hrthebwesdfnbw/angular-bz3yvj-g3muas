1d6bb42a984da5e86c2657bbb41477f25c11adf6
hrthebwesdfnbw <3345887119@qq.com>
2019-07-10 10:27:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bz3yvj-g3muas
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bz3yvj-g3muas)
---
+++ app.module.ts
@@ -0,0 +1,37 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { DataSourceBackendExampleComponent } from './src/data-source-backend-example.component'
+
+@Component({
+    selector: 'app-component',
+    template: '<data-source-backend-example></data-source-backend-example>'
+})
+export class AppComponent {}
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgSelectModule,
+        NgOptionHighlightModule,
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        DataSourceBackendExampleComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
+
